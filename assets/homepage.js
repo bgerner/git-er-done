9db33fa52cbbb7f68df72b573572a34c570a5aef
Code Wolf <brgerner@gmail.com>
2021-04-22 16:08:55 -0700

    added ability to sort by language (independent of username search)

--- assets/homepage.js
+++ assets/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.getElementById('user-form');
 var nameInputEl = document.getElementById('username');
 var repoSearchTerm = document.getElementById('repo-search-term');
 var repoContainerEl = document.getElementById('repos-container');
+var languageButtonsEl = document.getElementById('language-buttons')
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -82,6 +83,32 @@ var displayRepos = function (repos, searchTerm) {
         // append container to the dom
         repoContainerEl.appendChild(repoEl);
     }
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        }
+        else {
+            alert('Error: GotHub User Not Found')
+        }
+    })
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language")
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
 }
 
-userFormEl.addEventListener("submit", formSubmitHandler)
+userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler)
